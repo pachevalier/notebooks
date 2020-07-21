3e499f98dc91c493373089c030f6d9e3a59f0180
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2020-07-21 10:10:43 +0200

    the script get short names of certified organizations and short name of datasets from certified organizations

--- datagouv-sigles.R
+++ datagouv-sigles.R
@@ -1,6 +1,44 @@
 library("tidyverse")
 library("httr")
-get_sigle <- function(id) {
+
+get_sigle_organization <- function(id) {
+  tibble(
+    id = id, 
+    term = paste0("https://www.data.gouv.fr/api/1/organizations/", id, "/") %>%
+      GET() %>% 
+      content() %>%
+      pluck("acronym") %||% ""
+  )
+}
+get_sigle_organization(id = "5ee8ce78f450050a4fa6a1d3") 
+
+table_organizations <- "https://www.data.gouv.fr/fr/datasets/r/b7bbfedc-2448-4135-a6c7-104548d396e7" %>%
+  read_csv2()
+
+table_sigles_organizations <- table_organizations %>%
+  filter(badges != "[]" ) %>%
+  select(id) %>% 
+  pluck("id") %>%
+  map_df(.f = get_sigle_organization) %>%
+  filter(term != "") 
+
+table_organizations %>%
+  inner_join(
+    by = "id", 
+    y = table_sigles_organizations
+    ) %>%
+  mutate(
+    source = "Datagouv", 
+    url_source = paste0("https://www.data.gouv.fr/api/1/organizations/", id, "/")
+  ) %>%
+  select(term, definition = name, source, url_source) %>%
+  arrange(term) %>%
+  write_csv(path = "datagouv-organizations-sigles.csv")
+
+read_csv("datagouv-organizations-sigles.csv") %>%
+  glimpse()
+
+get_sigle_dataset <- function(id) {
   tibble(
     id = id, 
     term = paste0("https://www.data.gouv.fr/api/1/datasets/", id, "/") %>%
@@ -9,28 +47,32 @@ get_sigle <- function(id) {
     pluck("acronym") %||% ""
   )
 }
-get_sigle(id = "5c34c4d1634f4173183a64f1") 
-get_sigle(id = "5e870c8219cd4e437b686bac")
+get_sigle_dataset(id = "5c34c4d1634f4173183a64f1") 
+get_sigle_dataset(id = "5e870c8219cd4e437b686bac")
 
-table_sigles <- read_csv2("data-raw/export-dataset-20200711-074504.csv") %>%
-  filter(featured == TRUE) %>%
-  select(id) %>%
+table_datasets <- read_csv2("https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3")
+
+table_sigles_dataset <- table_datasets %>%
+  inner_join(
+    y = select(filter(table_organizations, badges != "[]"), id), 
+    by = c("organization_id" = "id")
+    ) %>%
   pluck("id") %>%
-  map_df(.f = get_sigle)
+  map_df(.f = get_sigle_dataset)
 
-read_csv2("data-raw/export-dataset-20200711-074504.csv") %>%
-  filter(featured == TRUE) %>%
-  select(id, title) %>%
+table_datasets %>%
+  inner_join(
+    y = select(filter(table_organizations, badges != "[]"), id), 
+    by = c("organization_id" = "id")
+  ) %>%
   left_join(
-    y = table_sigles, 
+    y = table_sigles_dataset, 
     by = "id"
     ) %>%
   filter(term != "") %>%
   mutate(
     source = "Datagouv", 
-    url_source = paste0("https://www.data.gouv.fr/fr/datasets/", id, "/")
+    url_source = paste0("https://www.data.gouv.fr/api/1/datasets/", id, "/")
     ) %>%
   select(term, definition = title, source, url_source) %>%
   write_csv(path = "datagouv-datasets-sigles.csv")
-
-
